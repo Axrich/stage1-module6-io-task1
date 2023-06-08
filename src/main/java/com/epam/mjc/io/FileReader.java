4027ff685d5de295cf893cba6929ed17b1fa71b6
sergey3321 <mail.sergey21@gmail.com>
2023-06-08 16:06:50 +0300

    stage1-module6-io-task1

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,30 @@
 package com.epam.mjc.io;
 
-import java.io.File;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
+        FileInputStream inputStream = null;
+        try {
+            inputStream = new FileInputStream("D://mjcSchoolTrainingFromEpamProjects2/stage1-module6-io-task1/src/main/resources/Profile.txt");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+             try {
+                if (inputStream == null) {
+                    inputStream.close();
+                }
+             } catch (IOException e) {
+                e.printStackTrace();
+             }
+        }
+
         return new Profile();
     }
 }
