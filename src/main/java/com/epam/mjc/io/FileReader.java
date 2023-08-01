f88cf62160744efa40f346d576d86dffdc5c3d42
sergey3321 <mail.sergey21@gmail.com>
2023-08-01 16:49:29 +0300

    stage1-module6-io-task1

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,5 +1,4 @@
 package com.epam.mjc.io;
-
 import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
@@ -7,22 +6,19 @@ import java.util.Map;
 
 public class FileReader {
 
+    String line = null;
+    Map<String, String> result = new HashMap<>();
+    String[] array = {};
     public Profile getDataFromFile(File file) {
 
-        String line = null;
-        Map<String, String> result = new HashMap<>();
-        String[] array = {};
-
-        try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("D://mjcSchoolTrainingFromEpamProjects2/stage1-module6-io-task1/src/main/resources/Profile.txt")));
-
-            while ((line = reader.readLine()) != null) {
-                array = line.split(":");
-                result.put(array[0], array[1]);
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("D://mjcSchoolTrainingFromEpamProjects2/stage1-module6-io-task1/src/main/resources/Profile.txt"))))
+            {
+                 while ((line = reader.readLine()) != null) {
+                 array = line.split(":");
+                 result.put(array[0], array[1]);
             }
+                System.out.println(result);
 
-            System.out.println();
-            reader.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -30,22 +26,4 @@ public class FileReader {
         return new Profile(result.get("Name"), Integer.parseInt(result.get("Age").trim()), result.get("Email"), Long.parseLong(result.get("Phone").trim()));
     }
 
-    public static void main(String[] args) throws IOException {
-
-        String line;
-        String[] array;
-        try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("D://mjcSchoolTrainingFromEpamProjects2/stage1-module6-io-task1/src/main/resources/Profile.txt")));
-            Map<String, String> result = new HashMap<>();
-
-            while ((line = reader.readLine()) != null) {
-                array = line.split(":");
-                result.put(array[0], array[1]);
-            }
-            System.out.println(result);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 }
