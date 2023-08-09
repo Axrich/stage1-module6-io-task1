da5e09658d705dd50bc1d15c4f31b6df2f9e796a
sergey3321 <mail.sergey21@gmail.com>
2023-08-09 15:41:56 +0300

    stage1-module6-io-task1

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -2,28 +2,26 @@ package com.epam.mjc.io;
 import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
-
+import java.util.logging.*;
 
 public class FileReader {
-
     String line = null;
     Map<String, String> result = new HashMap<>();
     String[] array = {};
     public Profile getDataFromFile(File file) {
 
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("D://mjcSchoolTrainingFromEpamProjects2/stage1-module6-io-task1/src/main/resources/Profile.txt"))))
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file))))
             {
                  while ((line = reader.readLine()) != null) {
                  array = line.split(":");
                  result.put(array[0], array[1]);
             }
-                System.out.println(result);
 
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        return new Profile(result.get("Name"), Integer.parseInt(result.get("Age").trim()), result.get("Email"), Long.parseLong(result.get("Phone").trim()));
+        return new Profile(result.get("Name").trim(), Integer.parseInt(result.get("Age").trim()), result.get("Email").trim(), Long.parseLong(result.get("Phone").trim()));
     }
 
 }
