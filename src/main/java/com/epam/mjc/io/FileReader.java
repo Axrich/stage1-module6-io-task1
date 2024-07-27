67ad4e560e0a8a25dbdf651ab38a15c05a4bdbc5
Axrich <116589250+Axrich@users.noreply.github.com>
2024-07-27 13:45:01 +0500

    Update FileReader.java

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -2,26 +2,28 @@ package com.epam.mjc.io;
 import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.logging.*;
 
 public class FileReader {
-    String line = null;
-    Map<String, String> result = new HashMap<>();
-    String[] array = {};
     public Profile getDataFromFile(File file) {
+        String line;
+        Map<String, String> result = new HashMap<>();
 
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file))))
-            {
-                 while ((line = reader.readLine()) != null) {
-                 array = line.split(":");
-                 result.put(array[0], array[1]);
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file)))) {
+            while ((line = reader.readLine()) != null) {
+                String[] array = line.split(":");
+                if (array.length == 2) {
+                    result.put(array[0].trim(), array[1].trim());
+                }
             }
-
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        return new Profile(result.get("Name").trim(), Integer.parseInt(result.get("Age").trim()), result.get("Email").trim(), Long.parseLong(result.get("Phone").trim()));
+        return new Profile(
+            result.get("Name").trim(),
+            Integer.parseInt(result.get("Age").trim()),
+            result.get("Email").trim(),
+            Long.parseLong(result.get("Phone").trim())
+        );
     }
-
 }
